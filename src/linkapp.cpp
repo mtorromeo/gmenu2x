1adddd51284d99daa323571207677c546f868115
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2006-10-22 20:42:11 +0000

    Chmod links with no exec or read permission (thanks to kasper sandberg)
    git-svn-id: https://gmenu2x.svn.sourceforge.net/svnroot/gmenu2x@53 bad23c4b-591a-0410-b480-ec8053e4ca07

--- src/linkapp.cpp
+++ src/linkapp.cpp
@@ -253,6 +253,19 @@ void LinkApp::run(string selectedFile, int startSelection) {
 
 		//check if we have to quit
 		string command = cmdclean(exec);
+		
+		// Check to see if permissions are desirable
+		struct stat fstat;
+		if( stat( command.c_str(), &fstat ) == 0 ) {
+			struct stat newstat = fstat;
+			if( S_IRUSR != ( fstat.st_mode & S_IRUSR ) )
+				newstat.st_mode |= S_IRUSR;
+			if( S_IXUSR != ( fstat.st_mode & S_IXUSR ) )
+				newstat.st_mode |= S_IXUSR;
+			if( fstat.st_mode != newstat.st_mode )
+				chmod( command.c_str(), newstat.st_mode );
+		} // else, well.. we are no worse off :)
+		
 		if (params!="") command += " " + params;
 		if (wrapper) command += "; sync & cd "+cmdclean(path)+"; exec ./gmenu2x";
 		if (dontleave) {
