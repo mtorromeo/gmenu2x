78798a85060155aa9c638ba88d9ed64389d4386c
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2010-09-18 22:38:25 +0000

    Added Dialog base class by Lars-Peter Clausen<lars@metafoo.de>
    git-svn-id: https://gmenu2x.svn.sourceforge.net/svnroot/gmenu2x@189 bad23c4b-591a-0410-b480-ec8053e4ca07

---
+++ src/dialog.cpp
@@ -0,0 +1,43 @@
+#include <string>
+
+#include "dialog.h"
+#include "gmenu2x.h"
+
+Dialog::Dialog(GMenu2X *gmenu2x) : gmenu2x(gmenu2x)
+{
+}
+
+void Dialog::drawTitleIcon(const std::string &icon, bool skinRes, Surface *s)
+{
+	if (s==NULL)
+		s = gmenu2x->s;
+
+	Surface *i = NULL;
+	if (!icon.empty()) {
+		if (skinRes)
+			i = gmenu2x->sc.skinRes(icon);
+		else
+			i = gmenu2x->sc[icon];
+	}
+
+	if (i==NULL)
+		i = gmenu2x->sc.skinRes("icons/generic.png");
+
+	i->blit(s,4,(gmenu2x->skinConfInt["topBarHeight"]-32)/2);
+}
+
+void Dialog::writeTitle(const std::string &title, Surface *s)
+{
+	if (s==NULL)
+		s = gmenu2x->s;
+	s->write(gmenu2x->font, title, 40, gmenu2x->skinConfInt["topBarHeight"]/4, SFontHAlignLeft, SFontVAlignMiddle);
+}
+
+void Dialog::writeSubTitle(const std::string &subtitle, Surface *s)
+{
+	if (s==NULL)
+		s = gmenu2x->s;
+	s->write(gmenu2x->font, subtitle, 40, gmenu2x->skinConfInt["topBarHeight"]/4*3, SFontHAlignLeft, SFontVAlignMiddle);
+}
+
+
