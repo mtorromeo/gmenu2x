22ab7240e9d4f635b2c99020680da3a890d564d6
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2010-09-18 19:26:27 +0200

    Replaced substr with compare

--- src/dirdialog.cpp
+++ src/dirdialog.cpp
@@ -181,7 +181,7 @@ bool DirDialog::exec() {
 
 void DirDialog::up() {
 	string::size_type p = path.rfind("/");
-	if (p==string::npos || path.substr(0,4)!="/mnt" || p<4) {
+	if (p==string::npos || path.compare(0,4,"/mnt")!=0 || p<4) {
 		close = true;
 		result = false;
 	} else {
