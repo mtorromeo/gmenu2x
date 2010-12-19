9978580d517688622c3a6e36f072a53552bd538f
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2010-12-19 21:47:13 +0100

    Fixed translation parameter termination

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -1423,7 +1423,7 @@ void GMenu2X::editLink() {
 	sd.addSetting(new MenuSettingMultiString(this,tr["Section"],tr["The section this link belongs to"],&newSection,&menu->getSections()));
 	sd.addSetting(new MenuSettingImage(this,tr["Icon"],tr.translate("Select an icon for the link: $1",linkTitle.c_str(),NULL),&linkIcon,".png,.bmp,.jpg,.jpeg"));
 	sd.addSetting(new MenuSettingFile(this,tr["Manual"],tr["Select a graphic/textual manual or a readme"],&linkManual,".man.png,.txt"));
-	sd.addSetting(new MenuSettingInt(this,tr.translate("Clock (default: $1)",strClock.c_str()),tr["Cpu clock frequency to set when launching this link"],&linkClock,50,confInt["maxClock"]));
+	sd.addSetting(new MenuSettingInt(this,tr.translate("Clock (default: $1)",strClock.c_str(), NULL),tr["Cpu clock frequency to set when launching this link"],&linkClock,50,confInt["maxClock"]));
 	sd.addSetting(new MenuSettingBool(this,tr["Tweak RAM Timings"],tr["This usually speeds up the application at the cost of stability"],&linkUseRamTimings));
 	sd.addSetting(new MenuSettingInt(this,tr["Volume (default: -1)"],tr["Volume to set for this link"],&linkVolume,-1,100));
 	sd.addSetting(new MenuSettingString(this,tr["Parameters"],tr["Parameters to pass to the application"],&linkParams, diagTitle,diagIcon));
