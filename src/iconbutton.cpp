e54c8b9395532af15627ec8a8e29866632756c57
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2011-04-24 00:06:20 +0200

    Unused variable

--- src/iconbutton.cpp
+++ src/iconbutton.cpp
@@ -30,9 +30,6 @@ void IconButton::setPosition(int x, int y) {
 }
 
 void IconButton::paint() {
-	uint margin = labelMargin;
-	if (iconSurface == NULL || label == "")
-		margin = 0;
 	if (iconSurface != NULL)
 		iconSurface->blit(gmenu2x->s,iconRect);
 	if (label != "")
